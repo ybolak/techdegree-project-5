a908321f1b55be171281145c2b8fdeb14e7481c7
Yuko Bolak <yukobolak@gmail.com>
2022-07-05 18:25:42 -0400

    Edited css and created a search function.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-baguetteBox.run('.gallery');​
+// baguetteBox.run('.gallery');​
+
+function search_photo() {
+    let input, filter, div, a_1, a_2, i, txtValue;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    div = document.getElementsByClassName('gallery');
+    a_1 = div.getElementsByTagName('a');
+
+
+    for( i = 0; i < a.length; i++ ) {
+        a_2 = a_1[j].getElementsByTagName('a')[0];
+        txtValue = j.textContent || j.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[j].style.display = "";
+        } else {
+            a[j].style.display = "none";
+        }
+    }
+}
+
