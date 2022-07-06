b9fddeec6b8e6a42864202483cd4cc8237954449
Yuko Bolak <yukobolak@gmail.com>
2022-07-06 18:28:47 -0400

    Updated search function in javascript.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-// baguetteBox.run('.gallery');
+baguetteBox.run('.gallery');
 
-const search = new Filter('search', 'data-caption');
+// credit The Net Ninja (https://www.youtube.com/watch?v=3NG8zy0ywIk)
+const searchBar = document.querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const pictures = document.getElementsByTagName('a');
+    Array.from(pictures).forEach(function(pic){
+        const caption = pic.dataset.caption;
+        if(caption.toLowerCase().indexOf(term) != -1 ){
+            pic.style.display = 'block';
+        } else {
+            pic.style.display = 'none';
+        }
+    })
+    
+})
+
+
+
+  
